8c4c808853ccf6bf94c0baf967c1846153d2ff71
Amir8-5 <amirbouryazadeh@gmail.com>
2024-02-01 20:33:55 -0500

    changed visibility in rpc.html and added computer choice in rpc.js

---
+++ rpc.js
@@ -0,0 +1,18 @@
+let getComputerChoice = function () {
+    let randomNum = Math.floor(Math.random() * (3) + 1);
+
+    switch (randomNum) {
+        case 1:
+            return "Rock";
+            break;
+        case 2:
+            return "Paper";
+            break;
+        case 3:
+            return "Scissors";
+            break;
+    }
+}
+
+
+
